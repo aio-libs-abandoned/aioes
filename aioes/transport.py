1dbe70b8470c25c2d694005d515077e4d521d70a
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-08-18 12:08:50 +0300

    Add perform_request transport method

--- aioes/transport.py
+++ aioes/transport.py
@@ -112,6 +112,7 @@ class Transport:
 
         self.hosts = hosts
 
+    @asyncio.coroutine
     def mark_dead(self, connection):
         """
         Mark a connection as dead (failed) in the connection pool. If sniffing
@@ -124,4 +125,73 @@ class Transport:
 
         self._connection_pool.mark_dead(connection)
         if self._sniff_on_connection_fail:
-            self._sniff_hosts()
+            yield from self.sniff_hosts()
+
+    @asyncio.coroutine
+    def perform_request(self, method, url, params=None, body=None):
+        """
+        Perform the actual request. Retrieve a connection from the connection
+        pool, pass all the information to it's perform_request method and
+        return the data.
+
+        If an exception was raised, mark the connection as failed and retry (up
+        to `max_retries` times).
+
+        If the operation was succesful and the connection used was previously
+        marked as dead, mark it as live, resetting it's failure count.
+
+        :arg method: HTTP method to use
+        :arg url: absolute url (without host) to target
+        :arg params: dictionary of query parameters, will be handed over to the
+            underlying :class:`~elasticsearch.Connection` class for serialization
+        :arg body: body of the request, will be serializes using serializer and
+            passed to the connection
+        """
+        if body is not None:
+            body = self.serializer.dumps(body)
+
+            # some clients or environments don't support sending GET with body
+            if method == 'GET' and self.send_get_body_as != 'GET':
+                # send it as post instead
+                if self.send_get_body_as == 'POST':
+                    method = 'POST'
+
+                # or as source parameter
+                elif self.send_get_body_as == 'source':
+                    if params is None:
+                        params = {}
+                    params['source'] = body
+                    body = None
+
+        if body is not None:
+            try:
+                body = body.encode('utf-8')
+            except UnicodeDecodeError:
+                # Python 2 and str, no need to re-encode
+                pass
+
+        ignore = ()
+        timeout = None
+        if params:
+            timeout = params.pop('request_timeout', None)
+            ignore = params.pop('ignore', ())
+            if isinstance(ignore, int):
+                ignore = (ignore, )
+
+        for attempt in range(self.max_retries + 1):
+            connection = self.get_connection()
+
+            try:
+                status, headers, data = connection.perform_request(method, url, params, body, ignore=ignore, timeout=timeout)
+            except ConnectionError:
+                self.mark_dead(connection)
+
+                # raise exception on last retry
+                if attempt == self.max_retries:
+                    raise
+            else:
+                # connection didn't fail, confirm it's live status
+                self.connection_pool.mark_live(connection)
+                if data:
+                    data = self.deserializer.loads(data, headers.get('content-type'))
+                return status, data
