8161ec1fc511ba948451ce121c863ca878ef482d
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-08-20 12:25:35 +0300

    Add test for round-robin selector

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -1,7 +1,7 @@
 import random
 import unittest
 
-from aioes.pool import RandomSelector
+from aioes.pool import RandomSelector, RoundRobinSelector
 
 
 class TestRandomSelector(unittest.TestCase):
@@ -14,5 +14,19 @@ class TestRandomSelector(unittest.TestCase):
 
     def test_select(self):
         s = RandomSelector()
+        r = s.select([1, 2, 3])
+        self.assertEqual(2, r)
+
+
+class TestRoundRobinSelector(unittest.TestCase):
+
+    def test_select(self):
+        s = RoundRobinSelector()
+        r = s.select([1, 2, 3])
+        self.assertEqual(2, r)
+        r = s.select([1, 2, 3])
+        self.assertEqual(3, r)
+        r = s.select([1, 2, 3])
+        self.assertEqual(1, r)
         r = s.select([1, 2, 3])
         self.assertEqual(2, r)
