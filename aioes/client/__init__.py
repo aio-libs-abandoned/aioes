f6c5cd7fbfa892ef4a88288f952e82d1281de4cc
Mike Nemkov <mike.nemkov@gmail.com>
2014-08-20 18:39:07 +0300

    add tests and methods: index, exists, get, delete

--- aioes/client/__init__.py
+++ aioes/client/__init__.py
@@ -1,10 +1,17 @@
+import asyncio
 import weakref
 
 from .indices import IndicesClient
+from aioes.transport import Transport
+from .utils import _make_path
+from aioes.exception import NotFoundError
+
+default = object()
 
 
 class Elasticsearch:
-    def __init__(self):
+    def __init__(self, endpoints, *, loop=None, **kwargs):
+        self._transport = Transport(endpoints, loop=loop, **kwargs)
         self._indices = weakref.ref(IndicesClient(self))
         # self._cluster = weakref.ref(ClusterClient(self))
         # self._cat = weakref.ref(CatClient(self))
@@ -34,6 +41,9 @@ class Elasticsearch:
     def __repr__(self):
         pass
 
+    def close(self):
+        self._transport.close()
+
     def ping(self):
         pass
 
@@ -43,14 +53,187 @@ class Elasticsearch:
     def create(self):
         pass
 
-    def index(self):
-        pass
+    @asyncio.coroutine
+    def index(self, index, doc_type, body, id=None, *,
+              consistency=default, op_type=default, parent=default,
+              refresh=default, replication=default, routing=default,
+              timeout=default, timestamp=default, ttl=default,
+              version=default, version_type=default, pretty=default,
+              format=default):
+        """
+        Adds or updates a typed JSON document in a specific index, making it
+        searchable.
+        `<http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-index_.html>`_
 
-    def exists(self):
-        pass
+        :arg index: The name of the index
+        :arg doc_type: The type of the document
+        :arg body: The document
+        :arg id: Document ID
+        :arg consistency: Explicit write consistency setting for the operation
+        :arg op_type: Explicit operation type (default: index)
+        :arg parent: ID of the parent document
+        :arg refresh: Refresh the index after performing the operation
+        :arg replication: Specific replication type (default: sync)
+        :arg routing: Specific routing value
+        :arg timeout: Explicit operation timeout
+        :arg timestamp: Explicit timestamp for the document
+        :arg ttl: Expiration time for the document
+        :arg version: Explicit version number for concurrency control
+        :arg version_type: Specific version type
+        :arg pretty:
+        :arg format: Format of the output, default 'detailed'
+        """
+        params = {}
+        if consistency is not default:
+            params['consistency'] = consistency
+        if op_type is not default:
+            params['op_type'] = op_type
+        if parent is not default:
+            params['parent'] = parent
+        if refresh is not default:
+            params['refresh'] = refresh
+        if replication is not default:
+            params['replication'] = replication
+        if routing is not default:
+            params['routing'] = routing
+        if timeout is not default:
+            params['timeout'] = timeout
+        if timestamp is not default:
+            params['timestamp'] = timestamp
+        if ttl is not default:
+            params['ttl'] = ttl
+        if version is not default:
+            params['version'] = version
+        if version_type is not default:
+            params['version_type'] = version_type
+        if pretty is not default:
+            params['pretty'] = pretty
+        if format is not default:
+            params['format'] = format
 
-    def get(self):
-        pass
+        _, data = yield from self._transport.perform_request(
+            'PUT' if id else 'POST',
+            _make_path(index, doc_type, id),
+            params=params,
+            body=body)
+
+        return data
+
+    @asyncio.coroutine
+    def exists(self, index, id, doc_type='_all', *, parent=default,
+               preference=default, realtime=default, refresh=default,
+               routing=default, pretty=default, format=default):
+        """
+        Returns a boolean indicating whether or not given document exists
+        in Elasticsearch.
+        `<http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-get.html>`_
+
+        :arg index: The name of the index
+        :arg id: The document ID
+        :arg doc_type: The type of the document (uses `_all` by default to
+            fetch the first document matching the ID across all types)
+        :arg parent: The ID of the parent document
+        :arg preference: Specify the node or shard the operation should be
+            performed on (default: random)
+        :arg realtime: Specify whether to perform the operation in realtime or
+            search mode
+        :arg refresh: Refresh the shard containing the document before
+            performing the operation
+        :arg routing: Specific routing value
+        :arg pretty:
+        :arg format: Format of the output, default 'detailed'
+        """
+        params = {}
+        if parent is not default:
+            params['parent'] = parent
+        if refresh is not default:
+            params['refresh'] = refresh
+        if preference is not default:
+            params['preference'] = preference
+        if routing is not default:
+            params['routing'] = routing
+        if realtime is not default:
+            params['realtime'] = realtime
+        if pretty is not default:
+            params['pretty'] = pretty
+        if format is not default:
+            params['format'] = format
+
+        try:
+            yield from self._transport.perform_request('HEAD', _make_path(index, doc_type, id), params=params)
+        except NotFoundError:
+            return False
+        return True
+
+    @asyncio.coroutine
+    def get(self, index, id, doc_type='_all', *,
+            _source=default, _source_exclude=default,
+            _source_include=default, fields=default,
+            parent=default, preference=default, realtime=default,
+            refresh=default, routing=default, version=default,
+            version_type=default, pretty=default, format=default):
+        """
+        Get a typed JSON document from the index based on its id.
+        `<http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-get.html>`_
+
+        :arg index: The name of the index
+        :arg id: The document ID
+        :arg doc_type: The type of the document (uses `_all` by default to
+            fetch the first document matching the ID across all types)
+        :arg _source: True or false to return the _source field or not, or a
+            list of fields to return
+        :arg _source_exclude: A list of fields to exclude from the returned
+            _source field
+        :arg _source_include: A list of fields to extract and return from the
+            _source field
+        :arg fields: A comma-separated list of fields to return in the response
+        :arg parent: The ID of the parent document
+        :arg preference: Specify the node or shard the operation should be
+            performed on (default: random)
+        :arg realtime: Specify whether to perform the operation in realtime or
+            search mode
+        :arg refresh: Refresh the shard containing the document before
+            performing the operation
+        :arg routing: Specific routing value
+        :arg version: Explicit version number for concurrency control
+        :arg version_type: Explicit version number for concurrency control
+        :arg pretty:
+        :arg format: Format of the output, default 'detailed'
+        """
+        params = {}
+        if _source is not default:
+            params['_source'] = _source
+        if _source_exclude is not default:
+            params['_source_exclude'] = _source_exclude
+        if _source_include is not default:
+            params['_source_include'] = _source_include
+        if fields is not default:
+            params['fields'] = fields
+        if parent is not default:
+            params['parent'] = parent
+        if refresh is not default:
+            params['refresh'] = refresh
+        if preference is not default:
+            params['preference'] = preference
+        if routing is not default:
+            params['routing'] = routing
+        if realtime is not default:
+            params['realtime'] = realtime
+        if version is not default:
+            params['version'] = version
+        if version_type is not default:
+            params['version_type'] = version_type
+        if pretty is not default:
+            params['pretty'] = pretty
+        if format is not default:
+            params['format'] = format
+
+        _, data = yield from self._transport.perform_request(
+            'GET',
+            _make_path(index, doc_type, id),
+            params=params)
+
+        return data
 
     def get_source(self):
         pass
@@ -79,8 +262,54 @@ class Elasticsearch:
     def clear_scroll(self):
         pass
 
-    def delete(self):
-        pass
+    @asyncio.coroutine
+    def delete(self, index, doc_type, id, *,
+               consistency=default, parent=default, refresh=default,
+               replication=default, routing=default, timeout=default,
+               version=default, version_type=default, pretty=default,
+               format=default):
+        """
+        Delete a typed JSON document from a specific index based on its id.
+        `<http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-delete.html>`_
+
+        :arg index: The name of the index
+        :arg doc_type: The type of the document
+        :arg id: The document ID
+        :arg consistency: Specific write consistency setting for the operation
+        :arg parent: ID of parent document
+        :arg refresh: Refresh the index after performing the operation
+        :arg replication: Specific replication type (default: sync)
+        :arg routing: Specific routing value
+        :arg timeout: Explicit operation timeout
+        :arg version: Explicit version number for concurrency control
+        :arg version_type: Specific version type
+        :arg pretty:
+        :arg format: Format of the output, default 'detailed'
+        """
+        params = {}
+        if consistency is not default:
+            params['consistency'] = consistency
+        if replication is not default:
+            params['replication'] = replication
+        if timeout is not default:
+            params['timeout'] = timeout
+        if parent is not default:
+            params['parent'] = parent
+        if refresh is not default:
+            params['refresh'] = refresh
+        if routing is not default:
+            params['routing'] = routing
+        if version is not default:
+            params['version'] = version
+        if version_type is not default:
+            params['version_type'] = version_type
+        if pretty is not default:
+            params['pretty'] = pretty
+        if format is not default:
+            params['format'] = format
+
+        _, data = yield from self._transport.perform_request('DELETE', _make_path(index, doc_type, id), params=params)
+        return data
 
     def count(self):
         pass
---
+++ tests/test_client.py
@@ -0,0 +1,95 @@
+import asyncio
+import unittest
+from aioes import Elasticsearch
+from aioes.exception import NotFoundError
+
+import pprint
+import ipdb
+
+pp = pprint.pprint
+
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        self._index = 'test_elasticsearch'
+        self._body = {"user": "kimchy",
+                     "post_date": "2009-11-15T14:12:12",
+                     "message": "trying out Elasticsearch"
+        }
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
+        self.cl = Elasticsearch([{'host': 'localhost'}], loop=self.loop)
+        self.addCleanup(self.cl.close)
+        try:
+            self.delete(self._index, '', '')
+        except NotFoundError:
+            return False
+
+    def tearDown(self):
+        self.loop.close()
+
+    def delete(self, index, doctype, id):
+        @asyncio.coroutine
+        def go():
+            data = yield from self.cl.delete(index, doctype, id)
+            return data
+        return self.loop.run_until_complete(go())
+
+    def make_rec(self, doctype, id, _body=None):
+        @asyncio.coroutine
+        def go():
+            body = self._body
+            if _body is not None:
+                body = _body
+            data = yield from self.cl.index(self._index, doctype, body, id)
+            return data
+        return self.loop.run_until_complete(go())
+
+    def test_index(self):
+        """ index """
+        data = self.make_rec('tweet', '1')
+        self.assertEqual(data['_index'], self._index)
+        self.assertEqual(data['_type'], 'tweet')
+        self.assertEqual(data['_id'], '1')
+        self.assertEqual(data['_version'], 1)
+        self.assertEqual(data['created'], True)
+        # test bulk version
+        data = self.make_rec('tweet', '1')
+        self.assertEqual(data['_index'], self._index)
+        self.assertEqual(data['_type'], 'tweet')
+        self.assertEqual(data['_id'], '1')
+        self.assertEqual(data['_version'], 2)
+        self.assertEqual(data['created'], False)
+
+    def test_exist(self):
+        """ exists """
+        @asyncio.coroutine
+        def go(id):
+            data = yield from self.cl.exists(self._index, id)
+            return data
+
+        id = '100'
+        # test non-exist
+        data = self.loop.run_until_complete(go(id))
+        self.assertFalse(data)
+        # test exist
+        self.make_rec('exist', id)
+        data = self.loop.run_until_complete(go(id))
+        self.assertTrue(data)
+
+    def test_get(self):
+        """ get """
+        @asyncio.coroutine
+        def go(id):
+            data = yield from self.cl.get(self._index, id)
+            return data
+
+        id = '200'
+        self.make_rec('test_get', id)
+        data = self.loop.run_until_complete(go(id))
+        self.assertEqual(data['_id'], id)
+        self.assertEqual(data['_index'], self._index)
+        self.assertEqual(data['_type'], 'test_get')
+        self.assertEqual(data['_version'], 1)
+        self.assertEqual(data['found'], True)
+        self.assertEqual(data['_source'], self._body)
