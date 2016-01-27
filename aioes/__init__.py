5070d1c63b52224930c0df26e61ca9f01b83eff4
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-01-27 14:43:17 +0200

    Bump to 0.3.0

--- aioes/__init__.py
+++ aioes/__init__.py
@@ -10,7 +10,7 @@ __all__ = ('Elasticsearch', 'ConnectionError', 'NotFoundError',
            'ConflictError', 'RequestError', 'TransportError')
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 version = __version__ + ' , Python ' + sys.version
 
