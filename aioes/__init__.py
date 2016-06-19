0c1541a51afad14eb85a7c117bc638b5a27b216a
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-06-19 15:06:22 +0300

    Update CHANGES, bump to 0.5.0

--- aioes/__init__.py
+++ aioes/__init__.py
@@ -10,7 +10,7 @@ __all__ = ('Elasticsearch', 'ConnectionError', 'NotFoundError',
            'ConflictError', 'RequestError', 'TransportError')
 
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 version = __version__ + ' , Python ' + sys.version
 
