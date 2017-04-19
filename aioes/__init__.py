56f2cefdaf17341065fdbef0cd21ed1dcb65666d
Alexey Popravka <alexey.popravka@horsedevel.com>
2017-04-19 22:05:27 +0300

    bump version: v0.7.1

--- aioes/__init__.py
+++ aioes/__init__.py
@@ -10,7 +10,7 @@ __all__ = ('Elasticsearch', 'ConnectionError', 'NotFoundError',
            'ConflictError', 'RequestError', 'TransportError')
 
 
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 version = __version__ + ' , Python ' + sys.version
 
